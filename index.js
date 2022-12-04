e596343009795fb8e8dfd455d7eab85e45dee8e4
fadimoayad <fadimoayad99@yahoo.com>
2022-12-04 21:42:35 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(skay='roller-skate'){
+return `This Saturday, I want to ${skay}!`
+   
+}
+function mondayWork(skay='go to the office'){
+    return `This Monday, I will ${skay}.`
+}
+
+function wrapAdjective(skay='*'){
+   return function (st="a hard worker"){
+   return `You are ${skay}${st}${skay}!`
+    }
+}
